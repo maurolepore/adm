f88a33c041bf1c7c34565267c27991f3bcfdfc5a
Mauro Lepore <maurolepore@gmail.com>
2023-02-03 13:33:43 +0000

    Prune

--- tests/testthat/test-adm.R
+++
@@ -1,8 +0,0 @@
-test_that("adds the expected foreign keys", {
-  tmp <- local_tempdir()
-  write_csv(tibble(x_id = 1, y_id = 2), path(tmp, "x.csv"))
-  write_csv(tibble(y_id = 2), path(tmp, "y.csv"))
-
-  dm <- adm(tmp)
-  expect_s3_class(dm, "dm")
-})
