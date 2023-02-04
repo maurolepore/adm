e4517a846e3509ae8b4c5b692d3c0182efce41ff
Mauro Lepore <maurolepore@gmail.com>
2023-02-04 17:42:10 +0000

    Relocate

--- R/abcd.R
+++ R/abcd.R
@@ -8,10 +8,10 @@
 abcd <- function() {
   dm(
     a = tibble(a_id = 1, a = 1),
-    b = tibble(b_id = 1, b = 1),
     a_b = tibble(a_b_id = 1, a_id = 1, b_id = 1),
-    c = tibble(c_id = 1, c = 1, d_id = 1),
+    b = tibble(b_id = 1, b = 1),
     b_c = tibble(b_c_id = 1, b_id = 1, c_id = 1),
+    c = tibble(c_id = 1, c = 1, d_id = 1),
     d = tibble(d_id = 1, d = 1)
   ) |> add_keys()
 }
--- tests/testthat/test-abcd.R
+++ tests/testthat/test-abcd.R
@@ -1,6 +1,6 @@
 test_that("outputs the expected data model", {
   dm <- abcd()
   expect_s3_class(dm, "dm")
-  expect_named(dm, c("a", "b", "a_b", "c", "b_c", "d"))
+  expect_named(dm, c("a", "a_b", "b", "b_c", "c", "d"))
   expect_equal(unname(dm::dm_nrow(dm)), rep(1, length(dm)))
 })
