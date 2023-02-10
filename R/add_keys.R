b22840d2c39d91d950a5b45b80230974f3aa17e5
Mauro Lepore <maurolepore@gmail.com>
2023-02-10 12:19:18 +0000

    Prune dependencies

--- R/add_keys.R
+++ R/add_keys.R
@@ -22,3 +22,60 @@ add_keys <- function(dm) {
     add_primary_keys() |>
     add_foreign_keys()
 }
+
+add_primary_keys <- function(dm) {
+  out <- dm
+  nm <- names(out)
+  pk <- set_names(paste0(nm, "_id"), nm)
+  for (i in seq_along(out)) {
+    table <- nm[[i]]
+    primary_key <- pk[[i]]
+    out <- dm_add_pk(out, !!table, columns = !!primary_key)
+  }
+  out
+}
+
+add_foreign_keys <- function(dm) {
+  out <- dm |>
+    add_fk() |>
+    warn_if_lacks_fk()
+  out
+}
+
+add_fk <- function(dm) {
+  fk <- enframe_fk(dm)
+  has_fk <- !identical(nrow(fk), 0L)
+  if (has_fk) {
+    for (i in seq_len(nrow(fk))) {
+      table_i <- fk$table[[i]]
+      columns_i <- fk$columns[[i]]
+      ref_table_i <- fk$ref_table[[i]]
+      dm <- dm_add_fk(dm, !!table_i, !!columns_i, !!ref_table_i)
+    }
+  }
+  dm
+}
+
+warn_if_lacks_fk <- function(dm) {
+  lacks_fk <- identical(nrow(dm::dm_get_all_fks(dm)), 0L)
+  if (lacks_fk) {
+    warning("Added no foreign key (found no candidate).")
+  }
+  invisible(dm)
+}
+
+enframe_fk <- function(dm) {
+  out <- dm |>
+    as.list() |>
+    map(names) |>
+    map(\(x) grep("_id$", x, value = TRUE)) |>
+    imap(\(x, names_x) setdiff(x, paste0(names_x, "_id"))) |>
+    keep(\(x) length(x) > 0L) |>
+    enframe(name = "table", value = "columns")
+
+  out |>
+    group_by(table) |>
+    reframe(columns = unlist(.data$columns)) |>
+    mutate(ref_table = gsub("_id$", "", .data$columns)) |>
+    filter(.data$ref_table %in% names(dm))
+}
